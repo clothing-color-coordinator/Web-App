7798cb31e349e5d0539a98721373c94bfcadc1da
Jason Few <omence11@gmail.com>
2019-02-04 10:09:54 -0800

    Migration and Database complete

---
+++ ColorScheme/ColorScheme/Data/ColorSchemeDbContext.cs
@@ -0,0 +1,27 @@
+﻿using ColorScheme.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ColorScheme.Data
+{
+    public class ColorSchemeDbContext : DbContext
+    {
+        public ColorSchemeDbContext(DbContextOptions<ColorSchemeDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+
+        public DbSet<User> User { get; set; }
+
+        public DbSet<ColorSchemeM> colorScheme { get; set; }
+
+    }
+}
