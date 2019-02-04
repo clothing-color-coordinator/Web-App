b4ab106f6671fd2f1552b736e0b0e9a6ff398649
Jason Few <omence11@gmail.com>
2019-02-04 09:10:37 -0800

    Schema models complete

---
+++ ColorScheme/ColorScheme/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace ColorScheme.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+
+        public string Name { get; set; }
+    }
+}
