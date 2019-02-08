1e8f630281a01d33925f05f1fcf5f3a1b4a9146e
Deziree' Teague <dez.teague@gmail.com>
2019-02-07 18:07:39 -0800

    carlos pushed to master

---
+++ ColorScheme/ColorScheme/Migrations/20190208015640_carlos20190207.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ColorScheme.Migrations
+{
+    public partial class carlos20190207 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
